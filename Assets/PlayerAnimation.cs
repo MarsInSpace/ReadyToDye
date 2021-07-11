03ce81ba6a072e5dfd840d4b836b1249740a7f7e
Finja Koehn <Finja.Koehn@Student.HTW-Berlin.de>
2021-07-11 20:13:11 +0200

    Revert "Merge branch 'master' of https://hivemind.htw-berlin.de/bitbucket/scm/tudi/2dgame_prototype"
    This reverts commit 5e46e89479a7490da8129cb0af0502f103b3519a, reversing
    changes made to f3e711fdd4005ea7847357e64ac4a57893033cc5.

--- Assets/PlayerAnimation.cs
+++ Assets/PlayerAnimation.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
+
     Animator Anim;
+   
+   // public Teleportation teleporterScript;
 
     public void Start()
     {
         Anim = GetComponent<Animator>();
-      
+      //  teleporterScript = GameObject.Find("TeleporterLilaB").GetComponent<Teleportation>();
     }
 
     public void Update()
@@ -18,19 +21,13 @@ public class PlayerAnimation : MonoBehaviour
         //Idle/breathing is standard
 
         //JUMP
-
-        if (GetComponent<PlayerController>().Active == true)
+        if(GetComponent<PlayerController>().Grounded && GetComponent<PlayerController>().SpaceKeyDown == false  && Input.GetKey(KeyCode.Space))
         {
+            Anim.SetTrigger("jumping");
+        }
 
-        if (GetComponent<PlayerController>().Grounded && GetComponent<PlayerController>().SpaceKeyDown == false  && Input.GetKey(KeyCode.Space) && GetComponent<PlayerController>().Active == true)
-        {
-
-                //Anim.Play("Base Layer.Jump, 0, 1f");
-                Anim.SetTrigger("jumping");
-            }
-
-            //WALKING
-            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) && GetComponent<PlayerController>().Active == true )
+        //Walking
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
             Anim.SetBool("isRolling", true);
         }
@@ -39,8 +36,26 @@ public class PlayerAnimation : MonoBehaviour
             Anim.SetBool("isRolling", false);
         }
 
-        }
+
+        //IN TELEPORT 
+        /*if (teleporterScript.justTP == false)
+        {
+            Anim.SetTrigger("teleporting");
+        }*/
+
 
     }
 
+    //IN TELEPORT 
+    
+   /* private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.transform.tag == "Teleporter" )
+        {
+            Anim.SetTrigger("teleporting");
+            
+        }
+    }*/
+  
+    
 }
