b8735cc26f26ac9ddf6d8cce3f454f0f3c3664ea
Luane Marie Kort <Luane.Kort@Student.HTW-Berlin.de>
2021-07-11 23:34:55 +0200

    fixed ui problems (hopefully)

---
+++ Assets/Scripts/UI Scripts/FontColorMaster.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FontColorMaster : MonoBehaviour
+{
+    public static FontColorMaster Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else
+            Destroy(this.gameObject);
+    }
+
+    public Color LightBlue = new Color(65, 60, 119, 255);
+    public Color DarkBlue = new Color(65, 60, 119, 255);
+    public Color Beige = new Color(235, 207, 146, 255);
+    public Color Orange = new Color(226, 166, 96, 255);
+    public Color Black = new Color(35, 32, 53, 255);
+    public Color White = new Color(226, 166, 96, 255);
+}
